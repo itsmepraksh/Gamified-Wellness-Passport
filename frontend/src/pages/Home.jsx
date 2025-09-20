91cbc61554e9de1f6168513dfb62e7d664441648
Kajal <kajal09786kumari@gmail.com>
2025-09-20 12:26:17 +0530

    feat:(20-09-2025)

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import React from "react";
+import "./Home.css"; // styling ke liye (optional)
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My React App 🚀</h1>
+      <p>This is the Home Page.</p>
+    </div>
+  );
+}
+
+export default Home;
